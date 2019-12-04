aa8bf6ef376954515cf10bc1c1cb601f11fc04a7
keeganridgley107 <keeganwridgley@gmail.com>
2019-12-04 11:31:11 -0700

    basic html for chat_page

--- app.py
+++ app.py
@@ -14,11 +14,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '^(p#PHi&^wF,W_KDTTH2#j' # needed to create instance of socketIO app 
 socketio = SocketIO(app) # socketIO wrapper 
 
-
+################## ROUTES ####################
 @app.route("/")
 def index():
     return "Hello, world!"
 
+
+@app.route("/chat")
+def chat():
+    return render_template('./chat_page.html')
 
 if __name__ == "__main__":
     socketio.run(app, debug=True) # socketIO wrapper starts flask server 
