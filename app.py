262df5d8405685ad1295e53af5e8bea8b77fcaef
keeganridgley107 <keeganwridgley@gmail.com>
2019-12-04 11:10:16 -0700

    added flask / socketIO run call

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ author: keegan ridgley
 date: 12/04/19 
 """
 
+from flask import Flask, render_template 
+from flask_socketio import SocketIO  
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '^(p#PHi&^wF,W_KDTTH2#j' # needed to create instance of socketIO app 
+socketio = SocketIO(app) # socketIO wrapper 
+
 
 def main():
-    pass
+    socketio.run(app) # socketIO wrapper starts flask server 
 
 
 if __name__ == "__main__":
